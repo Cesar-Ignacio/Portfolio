ab02876c31372dacd03757813ab1876c1f256b63
cesar Tello <aquinocesarignacio@gmail.com>
2024-03-18 00:07:42 -0300

    cambiosSeecciones

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,24 +4,19 @@ import { Spotlight } from '../components/ui/Spotlight'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useTranslation } from 'react-i18next';
 import ListaRedes from '../components/ListaRedes'
+import SeccionConteiner from '../components/SeccionConteiner';
 const Inicio = () => {
 
   const [t] = useTranslation("global")
 
   return (
-    <div id='home' className='scroll-mt-[6rem] grid  md:grid-cols-2 
-     mx-auto rounded-[2rem] dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative sm:gap-0  xl:w-[70%] md:h-[32rem] bg-red-700'>
 
+    <SeccionConteiner id={'home'} className='scroll-mt-[5rem] grid  md:grid-cols-2 md:h-[32rem]'>
       {/* Efecto en pantalla */}
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill='#088c96' />
 
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-firefly-950 bg-silver-200 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,#1a2b2b)] transition-all 
-      duration-300 ease-linear "></div>
-
       {/* Presentacion */}
-      <div className='justify-self-center self-center text-silver-950 
-       dark:text-silver-100 tracking-[1px] relative z-20 md:col-start-1 md:col-end-2  flex flex-col gap-5'>
+      <div className='justify-self-center self-center text-silver-950 dark:text-silver-100 tracking-[1px] relative z-20 md:col-start-1 md:col-end-2  flex flex-col gap-5'>
         <div className='flex flex-col '>
           <h1 className='titulo text-[3.8rem] md:text-[3.8rem] lg:text-[4.7rem] text-center md:text-start bg-clip-text xl:dark:text-transparent bg-gradient-to-bl from-silver-100 to-silver-400 font-bold font-poppins'>César Tello</h1>
           <div className='flex items-center '>
@@ -32,14 +27,11 @@ const Inicio = () => {
         </div>
         {/* Redes */}
         <ListaRedes className=' flex justify-start gap-5' />
-
       </div>
 
       {/* Avatar sm*/}
-      <div className='flex  items-center justify-self-center row-start-1 md:invisible 
-     md:col-start-2 md:col-end-3  md:pb-[1rem] lg:pb-[0rem] ' >
-        <Avatar className='border-[3px] border-white   shadow-shark-900 dark:shadow-fountain-blue-300  shadow-[0px_0px_10px] 
-          w-[12rem] h-[12rem] md:w-[17rem] md:h-[17rem] lg:w-[20rem] lg:h-[20rem] z-20 '>
+      <div className='flex  items-center justify-self-center row-start-1 md:invisible md:col-start-2 md:col-end-3  md:pb-[1rem] lg:pb-[0rem] ' >
+        <Avatar className='border-[3px] border-white   shadow-shark-900 dark:shadow-fountain-blue-300  shadow-[0px_0px_10px]   w-[12rem] h-[12rem] md:w-[17rem] md:h-[17rem] lg:w-[20rem] lg:h-[20rem] z-20 '>
           <AvatarImage src="https://res.cloudinary.com/drutrk0me/image/upload/v1710710811/samples/ah2yvhr7vniwii1sjlhf.png" className='object-cover' />
           <AvatarFallback>CA</AvatarFallback>
         </Avatar>
@@ -51,8 +43,8 @@ const Inicio = () => {
         <span className='rotate-[90deg] text-[#3d646290] font-poppins font-bold text-[15rem] absolute top-[12rem] right-[-19rem]'>CESAR</span>
         <span className='rotate-[90deg] text-[#3d64629a] font-poppins font-bold text-[12rem] absolute top-[12rem] right-[7rem] z-[-2]'>TELLO</span>
       </div>
+    </SeccionConteiner>
 
-    </div>
   )
 }
 
--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import proyecto from '../assets/proyecto.png'
 import portfolio from '../assets/portfolio.png'
 import bzst from '../assets/bzst.png'
-import './proyectos.css'
+import SeccionConteiner from '../components/SeccionConteiner'
 const Proyectos = () => {
 
     const backgroundStyle = {
@@ -12,17 +12,14 @@ const Proyectos = () => {
     };
 
     return (
-        <div className='xl:w-[70%] mx-auto container scroll-mt-[6rem] dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative] relative bg-red-500' id='pro'>
-            {/* Radial gradient for the container to give a faded look */}
-            <div className=" transition-all duration-300 ease-linear absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-[#1a2b2b] bg-silver-200 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,#1a2b2b)]"></div>
+        <SeccionConteiner id={'pro'} className='  scroll-mt-[4rem] '>
             {/* Titulo */}
             <div className='text-silver-950 dark:text-silver-100 tracking-[1px] relative z-20 '>
                 <h2 className='text-[2rem] md:text-[2.5rem] font-bold bg-clip-text xl:dark:text-transparent bg-gradient-to-bl from-silver-100 to-silver-400 font-poppins' >Proyectos</h2>
             </div>
             <div className='grid lg:grid-cols-3 gap-1 mt-4 '>
-                <div className='rounded-lg bg-silver-800 p-[4px] flex flex-col gap-2 z-10  h-fit self-center'>
+                <div className='rounded-lg bg-silver-800 p-[4px] flex flex-col gap-2 z-10 h-fit '>
                     <div className='w-full bg-silver-950 rounded-lg text-silver-50 p-2 flex gap-3'>
-
                         <svg viewBox="0 0 256 257" className='w-[1.2rem]' xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><defs><linearGradient x1="-.828%" y1="7.652%" x2="57.636%" y2="78.411%" id="a"><stop stop-color="#41D1FF" offset="0%" /><stop stop-color="#BD34FE" offset="100%" /></linearGradient><linearGradient x1="43.376%" y1="2.242%" x2="50.316%" y2="89.03%" id="b"><stop stop-color="#FFEA83" offset="0%" /><stop stop-color="#FFDD35" offset="8.333%" /><stop stop-color="#FFA800" offset="100%" /></linearGradient></defs><path d="M255.153 37.938 134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.875 37.958c-2.746-4.814 1.371-10.646 6.827-9.67l120.385 21.517a6.537 6.537 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62Z" fill="url(#a)" /><path d="M185.432.063 96.44 17.501a3.268 3.268 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.78l15.304-4.649c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028 72.516-144.72c1.215-2.423-.88-5.186-3.54-4.672l-25.505 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.35-1.37-4.583-3.769-4.113Z" fill="url(#b)" /></svg>
                         <svg className='w-[1.2rem]'
                             viewBox="0 0 256 154"
@@ -43,7 +40,7 @@ const Proyectos = () => {
                     </div>
                     <div className='relative rounded-lg overflow-hidden'>
                         <img className='rounded-lg' src={proyecto} alt="" />
-                        <div className='absolute w-full bottom-0 p-2 flex justify-between back' >
+                        <div className='absolute w-full bottom-0 p-2 flex justify-between bg-gradient-to-t from-black to-transparent' >
                             <span className='text-silver-50'>E-commerce</span>
                             <span className='text-silver-300'>2023</span>
                         </div>
@@ -73,14 +70,14 @@ const Proyectos = () => {
                     </div>
                     <div className='relative rounded-lg overflow-hidden'>
                         <img className='rounded-lg' src={portfolio} alt="" />
-                        <div className='absolute w-full bottom-0 p-2 flex justify-between back' >
+                        <div className='absolute w-full bottom-0 p-2 flex justify-between bg-gradient-to-t from-black to-transparent' >
                             <span className='text-silver-50'>Portfolio</span>
                             <span className='text-silver-300'>Desarrollo 2024</span>
                         </div>
                     </div>
                     <button className='w-full bg-silver-950 rounded-lg text-silver-50 p-2'>ver</button>
                 </div>
-                <div className='rounded-lg bg-silver-800 p-[4px] flex flex-col gap-2 z-10  h-fit self-center'>
+                <div className='rounded-lg bg-silver-800 p-[4px] flex flex-col gap-2 z-10  h-fit '>
                     <div className='w-full bg-silver-950 rounded-lg text-silver-50 p-2 flex gap-3'>
 
                         <svg viewBox="0 0 256 257" className='w-[1.2rem]' xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><defs><linearGradient x1="-.828%" y1="7.652%" x2="57.636%" y2="78.411%" id="a"><stop stop-color="#41D1FF" offset="0%" /><stop stop-color="#BD34FE" offset="100%" /></linearGradient><linearGradient x1="43.376%" y1="2.242%" x2="50.316%" y2="89.03%" id="b"><stop stop-color="#FFEA83" offset="0%" /><stop stop-color="#FFDD35" offset="8.333%" /><stop stop-color="#FFA800" offset="100%" /></linearGradient></defs><path d="M255.153 37.938 134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.875 37.958c-2.746-4.814 1.371-10.646 6.827-9.67l120.385 21.517a6.537 6.537 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62Z" fill="url(#a)" /><path d="M185.432.063 96.44 17.501a3.268 3.268 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.78l15.304-4.649c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028 72.516-144.72c1.215-2.423-.88-5.186-3.54-4.672l-25.505 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.35-1.37-4.583-3.769-4.113Z" fill="url(#b)" /></svg>
@@ -103,16 +100,15 @@ const Proyectos = () => {
                     </div>
                     <div className='relative rounded-lg overflow-hidden'>
                         <img className='rounded-lg' src={bzst} alt="" />
-                        <div className='absolute w-full bottom-0 p-2 flex justify-between back' >
+                        <div className='absolute w-full bottom-0 p-2 flex justify-between bg-gradient-to-t from-black to-transparent' >
                             <span className='text-silver-50'>Carrito Compras</span>
                             <span className='text-silver-300'>Agosto 2023</span>
                         </div>
                     </div>
                     <button className='w-full bg-silver-950 rounded-lg text-silver-50 p-2'>ver</button>
                 </div>
-
             </div>
-        </div>
+        </SeccionConteiner>
     )
 }
 
